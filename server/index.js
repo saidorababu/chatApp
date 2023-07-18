829bd91ae5fbb3cb7aab57513b7db285a972ec14
sai dorababu <khsd118194@gmail.com>
2023-07-18 14:26:46 +0530

    first commit

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ const server = http.createServer(app);
 
 const io = new Server(server,{
   cors:{
-    origin:["http://localhost:3000",'https://futurechatapp-34e9.onrender.com','https://futurechatapp-3yiy.onrender.com'],
+    origin:["http://localhost:3000",'https://futurechatapp-34e9.onrender.com','https://futurechatapp-3yiy.onrender.com','https://futurechatapp1.onrender.com'],
     methods:["GET","POST"]
   }
 });
