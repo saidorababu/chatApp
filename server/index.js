07c6c928324480bb37f07710d01a9296c909b7f8
sai dorababu <khsd118194@gmail.com>
2023-07-18 14:31:43 +0530

    second commmit

--- server/index.js
+++ server/index.js
@@ -6,6 +6,7 @@ import http from 'http';
 import cors from 'cors';
 
 const app = express();
+app.use(cors());
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename)
 app.use(express.static(path.join(__dirname+"/public")))
